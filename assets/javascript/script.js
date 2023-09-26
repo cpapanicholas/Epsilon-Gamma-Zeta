75923d7e914f6baa137a1ba1aed69b187804a135
Chris Papanicholas JR <cpapanicholas@gmail.com>
2023-09-25 20:11:29 -0500

    added html items and stlye.css items for hi score

---
+++ assets/javascript/script.js
@@ -0,0 +1,69 @@
+const quizData = [
+    {
+        question: "What element in html is for paragraphs?",
+        choices: ["Choice A", "Choice B", "Choice C", "Choice E"],
+        correctAnswer: 0,
+    },
+    {
+        question: "",
+        choices: ["Choice X", "Choice Y", "Choice Z", "Choice"],
+        correctAnswer: 1,
+    },
+    {
+        question: "Question 3?",
+        choices: ["Choice T", "Choice W", "Choice V"],
+        correctAnswer: 1,
+    },
+    {
+        question: "Question 4?",
+        choices: ["Choice R", "Choice S", "Choice D"],
+        correctAnswer: 1,
+    },
+    // Add more questions here
+];
+
+let timer = 60;
+
+function displayInquiry() {
+
+
+
+
+
+}
+
+function checkResponse() {
+
+
+
+}
+
+function startTimer() {
+
+
+
+}
+
+function decreaseTime() {
+
+
+
+
+
+
+}
+
+function calcScore() {
+
+
+
+
+}
+
+function hi_score() {
+
+
+
+}
+
+
