3d58007bf532669725ee42ba23d850152018f603
Chris Papanicholas JR <cpapanicholas@gmail.com>
2023-09-26 21:07:13 -0500

    trying to get checkResponse funtion to work, added more code to script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,23 +1,23 @@
 const quizData = [
     {
         question: "What element in html is for paragraphs?",
-        choices: ["Choice A", "Choice B", "Choice C", "Choice E"],
+        choices: ["<p>", "<a>", "<article>", "<h1>"],
         correctAnswer: 0,
     },
     {
-        question: "What",
-        choices: ["Choice X", "Choice Y", "Choice Z", "Choice"],
+        question: "What element in css is used to change the background color?",
+        choices: ["color", "backgroundcolor", "colorbackground", "ocapcity"],
         correctAnswer: 1,
     },
     {
-        question: "Question 3?",
-        choices: ["Choice T", "Choice W", "Choice V"],
+        question: "What element in html can be used to seperate parts of the body?",
+        choices: ["<span>", "<section>", "<header>", "<footer>"],
         correctAnswer: 1,
     },
     {
-        question: "Question 4?",
-        choices: ["Choice R", "Choice S", "Choice D"],
-        correctAnswer: 1,
+        question: "What element in html is used to link a css document?",
+        choices: ["<a>", "<p>", "<link>", "<href>"],
+        correctAnswer: 2,
     },
     // Add more questions here
 ];
@@ -26,7 +26,7 @@ let currentQuestion = 0;
 let score = 0;
 
 
-let timeRemaining = 35;
+let timeRemaining = 50;
 let timerId;
 let questionIndex=0;
 
@@ -36,36 +36,11 @@ let questionsEl= document.getElementById("questions");
 let questionEl= document.getElementById("question");
 let startButtonEl= document.getElementById("startButton");
 let startDivEl= document.getElementById("start-div");
-let scoreEl = document.getElementById("score")
+let scoreEl = document.getElementById("score");
 startButtonEl.onclick= startQuiz;
 
-function displayInquiry() {
-    prompt("You have 35 seconds to complete this quiz.  If you get a question wrong you lose 10 seconds to complete the quiz.  Are you ready to play?")
-
-
-
-
-
-}
-
-function checkResponse(event) {
-    const userResponse = event.target.textContent;
-    const correctAnswer = question[currentQuestion].answer;
-
-    if (selectedChoices === correctAnswer) {
-        score++;
-        .textContent = score;   
-
-
-
-
-
-
-    }
-
-
-
-}
+let quizStartPrompt = "Hi welcome to my coding quiz, you have 50 seconds to complete it.  For every question you get wrong you lose 10 seconds, if there isn't any time left you lose!  Are you ready?";
+const firstMessage = alert(quizStartPrompt);
 
 function startQuiz() {
     console.log("startButton click, start quiz function called");
@@ -79,19 +54,31 @@ function startQuiz() {
     getNextQuestion();
 }
 
-function oneSecHandler() {
-    timeRemaining--;
-    timerEl.textContent=timeRemaining;
+function checkResponse(event) {
+    const selectedChoices = event.target.textContent;
+    const correctAnswer = question[currentQuestion].answer;
 
-    if(timeRemaining <=0) {
-        endQuiz();
-    }
+    if (selectedChoices === correctAnswer) {
+        score++;
+        scoreEl.textContent = score;  
+        currentQuestion++;  
+        if (currentQuestion < question.length){
+            showQuestion()
+        } else (selectedChoices !== correctAnswer); {
+                timeLeft = -10
+        
+            }
+        
+
+    
+
+
+    currentQuestion++;
+
+    if (currentQuestion < question.length);
+    
 }
-
-function endQuiz() {
-    //tbd
 }
-
 function getNextQuestion() {
     let question= quizData [questionIndex];
     questionEl.textContent= question.question;
@@ -109,7 +96,38 @@ function getNextQuestion() {
 
 }
 
+function oneSecHandler() {
+    timeRemaining--;
+    timerEl.textContent=timeRemaining;
+
+    if(timeRemaining <=0) {
+        endQuiz();
+    }
+}
+
+function endQuiz() {
+    if(timeRemaining === 0) {
+    const retry = confirm("You ran out of time!  Would you like to try again?");
+    if (retry) {
+        restartQuiz();
+    } else {
+        alert("Quiz Over! Your score: ${score}")
+    } else {
+        alert("Quiz Completed! Your score: ${score}")
+    }
+
+    
+    
+
+
+
+    }
+}
+
+
+
 function decreaseTime() {
+
 
 
 
